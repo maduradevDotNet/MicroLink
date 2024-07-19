7fa48faed7d0420503a8914170c5468c44a9b297
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-20 04:09:54 +0530

    coupon seed included

---
+++ Microlink.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microlink.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microlink.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { 
+                    CouponId=1,
+                    CouponCode="100FF",
+                    DiscountAmount=10,
+                    MinAmount=10,
+                },
+                new Coupon
+                {
+                    CouponId = 2,
+                    CouponCode = "200FF",
+                    DiscountAmount = 20,
+                    MinAmount = 20,
+                },
+                new Coupon
+                {
+                    CouponId = 3,
+                    CouponCode = "300FF",
+                    DiscountAmount = 30,
+                    MinAmount = 30,
+                }
+                );
+        }
+    }
+}
---
+++ Microlink.Services.CouponAPI/Migrations/20240719222905_AddSeedToDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Microlink.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "100FF", 10.0, 10 },
+                    { 2, "200FF", 20.0, 20 },
+                    { 3, "300FF", 30.0, 30 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 3);
+        }
+    }
+}
