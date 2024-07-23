39df6d316f338dfd16fc4c3e76919c52cc123bca
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-23 09:00:33 +0530

    fully code

--- Microlink.Front/Controllers/CouponController.cs
+++ Microlink.Front/Controllers/CouponController.cs
@@ -2,27 +2,115 @@
 using Microlink.Front.Service;
 using Microlink.Front.Service.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Microlink.Front.Controllers
 {
     public class CouponController : Controller
     {
-		private readonly ICouponService _couponService;
+        private readonly ICouponService _couponService;
 
-		public CouponController(ICouponService couponService)
+        public CouponController(ICouponService couponService)
         {
-			_couponService=couponService;
+            _couponService = couponService;
+        }
 
-		}
         public async Task<IActionResult> Index()
         {
-            List<CouponDto?> List = new();
+            List<CouponDto?> list = new();
             ResponseDto response = await _couponService.GetCouponAsync();
-            if (response.ISSuccess == true) { 
-            
+            if (response != null && response.ISSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
             }
 
+            return View(list);
+        }
+
+        public IActionResult CouponCreate()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto response = await _couponService.CreateCouponAsync(model);
+                if (response != null && response.ISSuccess)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, response?.Message);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponEdit(int couponId)
+        {
+            ResponseDto response = await _couponService.GetCouponByIdAsync(couponId);
+            if (response != null && response.ISSuccess)
+            {
+                CouponDto model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponEdit(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto response = await _couponService.UpdateCouponAsync(model);
+                if (response != null && response.ISSuccess)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, response?.Message);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto response = await _couponService.GetCouponByIdAsync(couponId);
+            if (response != null && response.ISSuccess)
+            {
+                CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDeletea(int couponId)
+        {
+            ResponseDto response = await _couponService.DeleteCouponAsync(couponId);
+            if (response != null && response.ISSuccess)
+            {
+                TempData["success"] = "Coupon deleted successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- Microlink.Front/Models/CouponDto.cs
+++ Microlink.Front/Models/CouponDto.cs
@@ -3,10 +3,19 @@
 namespace Microlink.Front.Models
 {
     public class CouponDto
-    {    
+    {
         public int CouponId { get; set; }
+
+        [Required(ErrorMessage = "Coupon code is required")]
+        [StringLength(10, ErrorMessage = "Coupon code cannot exceed 10 characters")]
         public string? CouponCode { get; set; }
+
+        [Required(ErrorMessage = "Discount amount is required")]
+        [Range(0.01, 100.00, ErrorMessage = "Discount amount must be between 0.01 and 100.00")]
         public double DiscountAmount { get; set; }
+
+        [Required(ErrorMessage = "Minimum amount is required")]
+        [Range(0.01, 1000.00, ErrorMessage = "Minimum amount must be between 0.01 and 1000.00")]
         public int MinAmount { get; set; }
     }
 }
